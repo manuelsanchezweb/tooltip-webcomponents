0173cc3b00ca41085ed17e605ba09b1f17c0390d
manuelsanchez2 <manusansan22@gmail.com>
2022-12-21 09:11:45 +0100

    Add some accessibility improvements

--- tooltip.js
+++ tooltip.js
@@ -49,6 +49,10 @@ class TooltipIcon extends HTMLElement {
       .tooltip:focus .tooltip__message {
         display: block;
       }
+
+      .tooltip:focus {
+        outline: none !important;
+      }
       
       .tooltip__message::after {
         content: "";
@@ -146,7 +150,7 @@ class TooltipIcon extends HTMLElement {
     // Create the content element
     const content = document.createElement("div");
     content.innerHTML = `
-          <div class="tooltip">
+          <div class="tooltip" role="tooltip" tabindex="0">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-info"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line>
               </svg>
               <div class="tooltip__message tooltip--${this._direction}">
